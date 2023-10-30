d1e89e634dcc61528e39299ec28d5cd00d59f9e9
MarleneElla <ellamarlene54@gmail.com>
2023-10-30 11:49:51 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,35 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+
+  
+  useEffect(() => {
+    // Send a fetch request to the API
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(data => {
+        // Update the dogImage state with the received image URL
+        setDogImage(data.message);
+      });
+  }, []);
+
+  
+  
+  return (
+    <div>
+      {dogImage ? (
+        <img src={dogImage} alt="A Random Dog" />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
+
+
+export default App;
